a7d34d2784fd3dc9fe2fc70f1c934e2ce92265b4
TYSON WRIGHT <tjwrig@deakin.edu.au>
2020-11-30 20:09:57 +1100

    Adde dpath to static file storage

--- plantCatalogue/settings.py
+++ plantCatalogue/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'static'),
     ]
